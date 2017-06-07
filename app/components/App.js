ce0011d697249b7ac1140a64f5eb53b474e795fd
Steven <ssimonitch@gmail.com>
2017-06-06 17:05:24 -0700

    initial commit

---
+++ app/components/App.js
@@ -0,0 +1,36 @@
+const React = require('react');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+
+const Home = require('./Home');
+const Nav = require('./Nav');
+const Popular = require('./Popular');
+const Battle = require('./Battle');
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <div className="container">
+          <Nav />
+          {/* Switch renders route in order and defaults to last case if no routes avail */}
+          <Switch>
+            {/* exact requires that path must match what is specified to be active */}
+            <Route exact path="/" component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            {/* default path will render for as long as path is active */}
+            <Route path='/popular' component={Popular} />
+            {/* render UI if path not found */}
+            <Route render={() => {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+module.exports = App;
---
+++ app/components/Popular.js
@@ -0,0 +1,112 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+
+const api = require('../utils/api');
+
+// RENDERS LIST OF LANGUAGES
+function SelectLanguage (props) {
+  let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="languages">
+      {languages.map(lang => {
+        return <li
+          style={lang === props.selectedLanguage ? { color: '#d0021b'} : null}
+          onClick={props.onSelect(.bind(null, )lang)}
+          key={lang}>
+          {lang}
+        </li>
+      })}
+    </ul>
+  )
+}
+
+// RENDERS REPO GRID
+function RepoGrid (props) {
+  return (
+    <ul className="popular-list">
+      {props.repos.map((repo, index) => {
+        return (
+          <li key={repo.name} className="popular-item">
+            <div className="popular-rank">#{index + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img
+                  className="avatar"
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+// CHECK PROP TYPES
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
+
+// POPULAR CONTAINER
+class Popular extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null,
+    };
+
+    // binds the `this` keyword of `updateLanguage()` to the keyword passed in
+    // now `updateLanguage` will always be called with the correct `this` keyword
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage(lang) {
+    this.setState(() => {
+      return {
+        selectedLanguage: lang,
+        repos: null
+      }
+    });
+
+    api.fetchPopularRepos(lang)
+      .then(repos => this.setState(() => {
+        return {
+          repos: repos
+        }
+      }));
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+        {!this.state.repos
+          ? <p>LOADING</p>
+          : <RepoGrid repos={this.state.repos} />
+        }
+      </div>
+    )
+  }
+}
+
+module.exports = Popular;
